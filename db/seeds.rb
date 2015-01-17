6e2c4141bba9c84f7e7a7e538f2aca1d5bbbfcc0
cbfif47 <browder.c@gmail.com>
2015-01-17 19:38:19 +0000

    trying to fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-preroll = Preroll.create(date: 1/13/15, action_id: 1, loc1: 1, loc2:1)
+preroll = Preroll.create( action_id: 1, loc1: 1, loc2:1)
 blankloc = Location.create(name: "", available: false, active: false)
 actions = Action.create([{action: 'Deploy', impact: -1}, {action: 'Receive', impact: 1}, {action: 'Transfer', impact: 0}])
